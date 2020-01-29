dc90ab0fcbe1b10b1e97a321625041267d246262
Lorenzo <lorenzorheinicke@gmail.com>
2020-01-29 09:01:20 +0200

    feat: add more widgets to screen.

--- todoye/lib/screens/tasks_screen.dart
+++ todoye/lib/screens/tasks_screen.dart
@@ -64,7 +64,52 @@ class TasksScreen extends StatelessWidget {
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: null,
+        onPressed: () {
+          showModalBottomSheet(
+              context: context,
+              builder: (context) => Container(
+                    color: Color(0xff757575),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(20),
+                          topRight: Radius.circular(20),
+                        ),
+                      ),
+                      height: 300,
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          children: <Widget>[
+                            Text(
+                              "Add Task",
+                              style: TextStyle(
+                                color: Colors.lightBlueAccent,
+                                fontSize: 28,
+                                fontWeight: FontWeight.w700,
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(
+                                  bottom:
+                                      MediaQuery.of(context).viewInsets.bottom),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                    border: UnderlineInputBorder(),
+                                    helperText: "Enter task to do"),
+                              ),
+                            ),
+                            FlatButton(
+                              child: Text('Add task'),
+                              onPressed: null,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ));
+        },
         backgroundColor: Colors.lightBlueAccent,
       ),
     );
